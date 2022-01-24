23ff076485c49b5c925a0cd3bcd5e2d36551c0bf
sumit27b <87543002+sumit27b@users.noreply.github.com>
2022-01-24 15:47:58 +0530

    eclipse

---
+++ src/test/java/Eclipse.java
@@ -0,0 +1,13 @@
+
+public class Eclipse {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(22));
+		// Testing in the main method
+	}
+	public static boolean checkIfInputIsAnEvenNumber(int number){
+		return number % 2 == 0;
+
+	}
+
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+	@Test
+	public void testInputIsEven(){
+		assertTrue(Eclipse.checkIfInputIsAnEvenNumber(22)); 
+	}
+}
